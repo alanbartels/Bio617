f70a2a8839f23eb2af6993356da72841c868a647
Alan_Bartels_PC <alanbartels@gmail.com>
2022-09-20 10:59:47 -0400

    Fri 9/16 in class adds

--- Inclass_Code/ggplot2_intro/ggplot2_intro.R
+++ Inclass_Code/ggplot2_intro/ggplot2_intro.R
@@ -9,6 +9,8 @@ library(ggplot2)
 library(palmerpenguins)
 library(visdat)
 library(ggridges)
+library(ggdist)
+library(ggthemes)
 
 # how to run a function from a library without loading it
 visdat::vis_miss(penguins)
@@ -84,3 +86,183 @@ bill_dist_xy +
 # OTOH - what about a jitter?
 bill_dist_xy + 
   geom_jitter(size = 3, alpha = 0.5)
+
+#geom_point
+bill_dist_xy +
+  geom_point()
+
+#geom_violin
+bill_dist_xy +
+  geom_violin()
+
+#geom_boxplot
+bill_dist_xy +
+  geom_boxplot() +
+  geom_jitter(size=3,
+              alpha =0.3)
+
+#geom_point
+bill_dist_xy +
+  geom_point()
+
+#geom_violin
+bill_dist_xy +
+  geom_violin()
+
+#stat_summary
+bill_dist_xy + 
+  stat_summary(size = 0.1)
+
+#geom_density_ridges
+bill_dist_xy +
+  geom_density_ridges(aes(fill = species), alpha = 0.5)
+
+#geom_density_ridges
+bill_dist_xy +
+  geom_density_ridges(alpha = 0.5, mapping = aes(fill = species))
+
+#geom_slabinterval
+bill_dist_xy + 
+  geom_dotsinterval(side = 'bottom', aes(fill = 'black'), alpha = 0.5) +
+  stat_halfeye(aes(fill = species),alpha = 0.5)
+
+# Stat - summarizes or calculates something behind the scenes
+ggplot(data = penguins,
+        aes(x = species, y = bill_depth_mm, color = species)) + 
+  stat_summary(fun.data = "mean_sdl",
+               geom = "linerange", size = 4,
+               color = "black") +
+  stat_summary(fun.data = "mean_se")
+
+
+#### Scatterplots ####
+
+pen_mass_length <- 
+  ggplot(data = penguins,
+         mapping = aes(x = body_mass_g,
+                       y = bill_length_mm,
+                       color = species))
+
+# basic scatter
+pen_scatter <-  pen_mass_length + 
+  geom_point(alpha = 0.6)
+
+pen_scatter
+
+#### Facet by data property ####
+pen_scatter + facet_wrap(vars(species))
+
+
+pen_scatter +
+  facet_wrap(vars(species, sex))
+
+#facet_grid
+pen_scatter + facet_grid(vars(island, species))
+
+#facet 
+pen_scatter + 
+  facet_grid(rows = vars(island),
+             cols = vars(species))
+
+ggplot(data = penguins, 
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm,
+                     color = sex)) +
+  geom_point() +
+  facet_grid(rows = vars(island),
+             cols = vars(species))
+
+# facets and scales
+pen_scatter + facet_wrap(vars(species),
+                         scales = "free_y")
+
+# removing NAs
+penguins_clean <- penguins[!is.na(penguins$bill_depth_mm),]
+
+#### Add a little spice ####
+
+pen_scatter
+
+## LABS()
+#' Publication Quality-ify this graph
+#' add a title
+#' rename axes
+#' captions
+pen_scatter_beautiful <-  pen_scatter + 
+  labs(title = "Penguin Bill Length by Mass",
+       subtitle = "Data from Palmer LTER",
+       x = "Body Mass (g)",
+       y = "Bill length (m)",
+       color = "Species of\nPenguin")
+
+
+# redundant coding with shape
+pen_scatter_beautiful <- pen_scatter_beautiful +
+  aes(shape = species) +
+  labs(shape = "Species of\nPenguin")
+
+# make it colorblind friendly
+pen_scatter_beautiful+ 
+  scale_color_manual(values = c("red", "blue", "orange"))
+
+pen_scatter_beautiful +
+  scale_color_brewer(type = "qual")
+
+pen_scatter_beautiful +
+  scale_color_brewer(palette = "Dark2")
+
+pen_scatter_beautiful <- pen_scatter_beautiful +
+  ggthemes::scale_color_colorblind()
+
+# whole package of themes
+pen_scatter_beautiful + 
+  ggthemes::theme_excel()
+
+pen_scatter_beautiful +
+  theme_minimal(base_size = 16, 
+                base_family = "Papyrus") 
+ # theme(panel.background = element_rect(fill = "blue"))
+
+# continuous scales
+
+pen_scatter + 
+  aes(color = bill_depth_mm) +
+  scale_color_viridis_c()
+
+#### Stats ####
+pen_mass_length +
+  geom_point()
+
+pen_mass_length +
+  stat_smooth()
+
+pen_mass_length +
+  stat_smooth(method = "lm") +
+  geom_point()
+
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm)) +
+  stat_bin_2d()
+
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g,
+                     y = bill_length_mm)) +
+  stat_density_2d_filled()
+
+
+# Load the bigfoot data
+# explore what is there
+
+#### Bigfoot ####
+
+# Or read in the data manually
+
+bigfoot <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-13/bigfoot.csv')
+head(bigfoot)
+str(bigfoot)
+summary(bigfoot)
+
+ggplot(data = bigfoot, mapping = aes(x = date, y = uv_index, )) + geom_point()
+
+
