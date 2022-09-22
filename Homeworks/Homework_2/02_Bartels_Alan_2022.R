ddb45c04664741896d9f8fff92de91cf17428010
Alan_Bartels_PC <alanbartels@gmail.com>
2022-09-22 11:05:44 -0400

    working on HW2 and week3 lectures

--- Homeworks/Homework_2/02_Bartels_Alan_2022.R
+++ Homeworks/Homework_2/02_Bartels_Alan_2022.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 library(forcats)
+library(ggridges)
 
 theme_set(theme_bw(base_size=12))
 
@@ -13,10 +14,36 @@ ice <- read_csv("http://biol607.github.io/homework/data/NH_seaice_extent_monthly
 head(ice)
 
 ice_extent <- ggplot(data = ice, 
-                     mapping = aes(group = Year,
+                     mapping = aes(group = Month_Name,
                                    x = Extent,
                                    y = Month_Name))
+ice_extent + geom_boxplot(fill = "turquoise")
 
-ice_extent
+ice_extent + geom_density_ridges(fill = "turquoise")
 
-ice_extent + geom_boxplot()
+cut_interval(1:10, n = 5)
+
+annual_ice_extent <- ggplot(data = ice, 
+                            mapping = aes(
+                              group = Year,
+                              x = Month_Name,
+                              y = Extent
+                            ))
+annual_ice_graph <- annual_ice_extent + geom_line()
+
+annual_ice_graph
+
+ice_by_month <- ggplot(data = ice, 
+                       mapping = aes(
+                         group = Month_Name,
+                         x = Year,
+                         y = Extent
+                       ))
+ice_by_month <-ice_by_month + geom_line()
+
+ice_by_month + facet_wrap(vars(cut_interval(Month, n = 4)), 
+                          labeller = "label_both"
+                          )
+   
+
+                
--- Inclass_Code/week3_lec/pipes_lecture.R
+++ Inclass_Code/week3_lec/pipes_lecture.R
@@ -1,2 +1,45 @@
 #' -----------------------------------------------------------------------
 #' 
+#' Pipes
+#' 
+#' -----------------------------------------------------------------------
+
+# sum 1 through 10 and turn into a character
+sum(1:10)
+
+# nesting
+as.character(sum(1:10))
+
+as.character(
+  sum(1:10)
+)
+
+# take sum of 1:10
+# turn into a character
+# paste this phrase "this is the answer"
+# print
+
+print(
+  paste("This is the answer",
+        as.character(
+          sum(1:10)
+        )
+  )
+)
+
+# Lots of objects
+my_answer <- sum(1:10)
+my_answer <- as.character(my_answer)
+my_answer <- paste("This is the answer", my_answer)
+print(my_answer)
+
+# Enter the pipe
+# take an output and pipe it as the first arguement ot the next function
+
+sum(1:10) |> #cmd-shift-m
+  as.character() |>
+  paste("is the answer") |>
+  print()
+
+#### If you don't know how to code something, write it out! ####
+
